c5e349998da4798e8e6b250aae594d74de0fd6d0
PedroRomaoDev <pedro7ntj@gmail.com>
2025-09-16 17:27:41 -0300

    feat: add 404 not found page

--- src/pages/not-found.jsx
+++ src/pages/not-found.jsx
@@ -1,5 +1,23 @@
+import { Link } from "react-router";
+
 const NotFoundPage = () => {
-  return <h1>404 - Page Not Found</h1>;
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center bg-background px-4 text-center text-foreground">
+      <h1 className="text-6xl font-bold text-primary-green md:text-9xl">404</h1>
+      <h2 className="mt-4 text-2xl font-semibold text-foreground md:text-4xl">
+        Página Não Encontrada
+      </h2>
+      <p className="text-md mt-2 text-muted-foreground md:text-lg">
+        Oops! A página que você está procurando não existe ou foi movida.
+      </p>
+      <Link
+        to="/"
+        className="mt-6 rounded-lg bg-primary px-6 py-3 font-semibold text-primary-foreground shadow-md transition-opacity duration-300 hover:opacity-90"
+      >
+        Voltar para a Home
+      </Link>
+    </div>
+  );
 };
 
 export default NotFoundPage;
