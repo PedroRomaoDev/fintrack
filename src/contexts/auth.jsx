6743891c370c68280c6e41a51075dce3497e03c1
PedroRomaoDev <pedro7ntj@gmail.com>
2025-09-16 19:27:50 -0300

    feat: add useAuthContext hook

--- src/contexts/auth.jsx
+++ src/contexts/auth.jsx
@@ -1,6 +1,6 @@
 import { useMutation } from "@tanstack/react-query";
 import axios from "axios";
-import { createContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { toast } from "sonner";
 
 import { api } from "@/lib/axios";
@@ -10,6 +10,8 @@ export const AuthContext = createContext({
   login: () => {},
   signup: () => {},
 });
+
+export const useAuthContext = () => useContext(AuthContext);
 
 export const AuthContextProvider = ({ children }) => {
   const [user, setUser] = useState(null);
