98441012552917b4b0d9bcc52ce44187d0b14f5a
PedroRomaoDev <pedro7ntj@gmail.com>
2025-09-17 14:50:08 -0300

    feat: ensure access token ir refreshed using refresh token on 401 error

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-import { LOCAL_STORAGE_ACCESS_TOKEN_KEY } from "@/constants/local-storage";
+import {
+  LOCAL_STORAGE_ACCESS_TOKEN_KEY,
+  LOCAL_STORAGE_REFRESH_TOKEN_KEY,
+} from "@/constants/local-storage";
 
 export const protectedApi = axios.create({
   baseURL: "http://localhost:8080/api",
@@ -19,3 +22,45 @@ protectedApi.interceptors.request.use((request) => {
   request.headers.Authorization = `Bearer ${accessToken}`;
   return request;
 });
+
+protectedApi.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const request = error.config;
+    // verificar se tenho um refresh token
+    const refreshToken = localStorage.getItem(LOCAL_STORAGE_REFRESH_TOKEN_KEY);
+    if (!refreshToken) {
+      return Promise.reject(error);
+    }
+
+    // verificar se o erro é 401 (não autorizado/token inválido)
+    if (
+      error.response?.status === 401 &&
+      !request._retry &&
+      !request.url.includes("users/refresh-token")
+    ) {
+      request._retry = true; // para evitar loop infinito
+      try {
+        // se eu tiver um refresh token, eu tento renovar o access token e refazer a requisição
+        const response = await protectedApi.post("users/refresh-token", {
+          refreshToken,
+        });
+        const newAccessToken = response.data.accessToken;
+        const newRefreshToken = response.data.refreshToken;
+
+        // salvar os novos tokens
+        localStorage.setItem(LOCAL_STORAGE_ACCESS_TOKEN_KEY, newAccessToken);
+        localStorage.setItem(LOCAL_STORAGE_REFRESH_TOKEN_KEY, newRefreshToken);
+
+        // refazer a requisição que falhou com o novo access token
+        request.headers.Authorization = `Bearer ${newAccessToken}`;
+        return protectedApi(request);
+      } catch (refreshError) {
+        localStorage.removeItem(LOCAL_STORAGE_ACCESS_TOKEN_KEY);
+        localStorage.removeItem(LOCAL_STORAGE_REFRESH_TOKEN_KEY);
+        console.error(refreshError);
+      }
+    }
+    return Promise.reject(error);
+  }, // será executada caso a resposta tenha erro -> status 400, 500 etc
+);
