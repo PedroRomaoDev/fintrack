c282b8c6ab24c3d6a22721d4524686440f18645b
PedroRomaoDev <pedro7ntj@gmail.com>
2025-09-23 17:19:22 -0300

    feat: ensure selected date is persisted on page reload

--- src/components/date-selection.jsx
+++ src/components/date-selection.jsx
@@ -1,13 +1,38 @@
-import { addMonths } from "date-fns";
-import { useState } from "react";
+import { addMonths, format } from "date-fns";
+import { useEffect, useState } from "react";
+import { useNavigate, useSearchParams } from "react-router";
 
 import { DatePickerWithRange } from "./ui/date-picker-with-range";
 
 const DateSelection = () => {
+  const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
   const [date, setDate] = useState({
-    from: new Date(),
-    to: addMonths(new Date(), 1),
+    from: searchParams.get("from")
+      ? new Date(searchParams.get("from") + "T00:00:00")
+      : new Date(),
+    to: searchParams.get("from")
+      ? new Date(searchParams.get("to") + "T00:00:00")
+      : addMonths(new Date(), 1),
   });
+
+  // 1- sempre que o state 'date' mudar, eu preciso persisti-lo na URL (?from&to=)
+  // 2- quando eu recarregar a página, eu preciso ler esses valores da URL e persistir o state com eles
+  useEffect(() => {
+    // early return
+    if (!date?.from || !date?.to) {
+      return;
+    }
+
+    const queryParams = new URLSearchParams();
+    queryParams.set("from", format(date.from, "yyyy-MM-dd"));
+    queryParams.set("to", format(date.to, "yyyy-MM-dd"));
+
+    if (date.from && date.to) {
+      navigate(`/?${queryParams.toString()}`);
+    }
+  }, [navigate, date]);
+
   return <DatePickerWithRange value={date} onChange={setDate} />;
 };
 
