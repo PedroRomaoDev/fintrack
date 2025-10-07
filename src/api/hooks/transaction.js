b7c2d2ad0ecd605d512dcd34860e84f7528dceb4
PedroRomaoDev <pedro7ntj@gmail.com>
2025-10-07 19:55:46 -0300

    feat: add initial transactions table

--- src/api/hooks/transaction.js
+++ src/api/hooks/transaction.js
@@ -18,6 +18,9 @@ export const useCreateTransaction = () => {
       queryClient.invalidateQueries({
         queryKey: getUserBalanceQueryKey({ userId: user.id }),
       });
+      queryClient.invalidateQueries({
+        queryKey: getTransactionsQueryKey({ userId: user.id }),
+      });
     },
     onError: (error) => {
       toast.error(
@@ -38,5 +41,6 @@ export const useGetTransactions = ({ from, to }) => {
   return useQuery({
     queryKey: getTransactionsQueryKey({ userId: user.id, from, to }),
     queryFn: () => TransactionService.getAll({ from, to }),
+    enabled: Boolean(from) && Boolean(to) && Boolean(user.id),
   });
 };
